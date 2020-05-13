2d353c41623da05b9cb94ffe9c89bdc6d1e59142
Jordis Man <jordisman@gmail.com>
2020-05-12 18:27:29 -0700

    Updates

---
+++ precache-manifest.b1cff974039c7d024cf79da9ee83b099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a9aab08408197f02aca9b3b9c3f999",
+    "url": "/new-app/index.html"
+  },
+  {
+    "revision": "5b38a889ebc6ccf20338",
+    "url": "/new-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c72ebf53c651e74bf8e9",
+    "url": "/new-app/static/js/2.78156329.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-app/static/js/2.78156329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b38a889ebc6ccf20338",
+    "url": "/new-app/static/js/main.5b8b0086.chunk.js"
+  },
+  {
+    "revision": "4a9496bb6da5024f1918",
+    "url": "/new-app/static/js/runtime-main.13eea192.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
